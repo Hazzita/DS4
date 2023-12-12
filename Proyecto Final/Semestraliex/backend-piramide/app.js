a9209284b36b70b4f189e51f2c96adfc966259b1
Hazzita <helloimrubenjaen@gmail.com>
2023-12-11 21:31:53 -0500

    Correcciones Semestral

--- Proyecto Final/Semestraliex/backend-piramide/app.js
+++ Proyecto Final/Semestraliex/backend-piramide/app.js
@@ -3,34 +3,37 @@ const cors = require('cors');
 const app = express();
 const port = 4000;
 
+// Middlewares (funciones que se ejecutan antes de que lleguen a las rutas)
 app.use(cors());
 app.use(express.json());
 
+// Rutas (endpoints) 
 app.get('/', (req, res) => {
     res.json({ message: 'Proyecto final piramide' });
 });
 
+// Rutas (endpoints)
 app.post('/piramide', (req, res) => {
     if (typeof localStorage === "undefined" || localStorage === null) {
         var LocalStorage = require('node-localstorage').LocalStorage;
         localStorage = new LocalStorage('./scratch');
     }
-
+// Guardar piramide
     const body = req.body.piramide;
-
     localStorage.setItem('piramide', body);
 
     res.json({ message: 'Piramide guardada' });
 });
+
 
 app.get('/piramide', (req, res) => {
     if (typeof localStorage === "undefined" || localStorage === null) {
         var LocalStorage = require('node-localstorage').LocalStorage;
         localStorage = new LocalStorage('./scratch');
     }
-
+    // Obtener piramide
     const piramide = localStorage.getItem('piramide');
-
+    // Respuesta
     if(piramide != null){
         res.json({ piramide: piramide });
     }else{
